b332852c2ec58b227d7ad60b4b9ce582c8597fc1
Mark Allen <mrallen1@yahoo.com>
2012-09-13 21:14:11 -0500

    Implement action generator and step()

--- pysillywalks/walk.py
+++ pysillywalks/walk.py
@@ -3,7 +3,8 @@ from random import randint, choice
 class Walk(object):
     def __init__(self, name):
         self.name = name
-        self.steps = randint(0,9)
+        self.steps = randint(1,10)
+        self.movements = None
 
     def __str__(self):
         return "<Walk: %s (%s)>" % (self.name, self.steps)
@@ -12,6 +13,20 @@ class Walk(object):
         return "<Walk: %s (%s)>" % (self.name, self.steps)
 
     def move(self):
-        actions = ['shamble', 'stagger', 'roll', 'crawl', 'stumble']
-        print " ".join([choice(actions) for x in range(self.steps)])
+        if self.movements:
+            print " ".join(self.movements)
+        else:
+            self._generate()
+            self.move()
 
+    def _generate(self):
+        actions = ['shamble', 'stagger', 'roll', 'crawl', 'stumble']
+        self.movements = [choice(actions) for x in range(self.steps)]
+
+    def step(self):
+        if self.movements:
+            s = self.movements.pop(0)
+            print s
+        else:
+            self._generate()
+            self.step()
